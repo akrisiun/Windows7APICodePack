dc856bee893f146d60e719cb59149d9111c5054f
AndriusK <andrius.k@sanitex.eu>
2016-07-11 09:52:39 +0300

    TaskBar mini shell project

---
+++ TaskBar/Taskbar/TabbedThumbnailClosedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace Microsoft.WindowsAPICodePack.Taskbar
+{
+    /// <summary>
+    /// Event args for when close is selected on a tabbed thumbnail proxy window.
+    /// </summary>
+    public class TabbedThumbnailClosedEventArgs : TabbedThumbnailEventArgs
+    {
+        /// <summary>
+        /// Creates a Event Args for a specific tabbed thumbnail event.
+        /// </summary>
+        /// <param name="windowHandle">Window handle for the control/window related to the event</param>        
+        public TabbedThumbnailClosedEventArgs(IntPtr windowHandle) : base(windowHandle) { }
+
+        /// <summary>
+        /// Creates a Event Args for a specific tabbed thumbnail event.
+        /// </summary>
+        /// <param name="windowsControl">WPF Control (UIElement) related to the event</param>        
+        public TabbedThumbnailClosedEventArgs(UIElement windowsControl) : base(windowsControl) { }
+    }
+}
